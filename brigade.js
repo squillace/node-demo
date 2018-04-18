b9e774511d1f2cb7fe8fff105ed31f0c6b688652
Matt Butcher <matt.butcher@microsoft.com>
2018-04-17 18:01:40 -0600

    Add github stuff to brigade.js

--- brigade.js
+++ brigade.js
@@ -38,4 +38,35 @@ events.on("image_push", (e, p) => {
 // This is for the outer dev loop
 events.on("push", (e, p) => {
   console.log("Received GitHub push event")
+
+  // Note that we are running the source as-is, not inside of the iamge
+  // created. We could do this either way. I just wanted to illustrate the
+  // flexibility of this system.
+  const tests = new Job("gh-tests", "node:9-alpine")
+  tests.tasks = [
+    "cd /app",
+    "yarn install",
+    "yarn test"
+  ]
+
+  tests.run().then( ()=> {
+    console.log("tests completed successfully")
+    ghNotify("success", "Passed", e, p).run()
+  }).catch((e) => {
+    console.log("tests failed")
+    ghNotify("failure", `failed: ${e.toString()}`, e, p).run()
+  })
 });
+
+function ghNotify(state, msg, e, project) {
+  const gh = new Job(`notify-${ state }`, "technosophos/github-notify:latest")
+  gh.env = {
+    GH_REPO: project.repo.name,
+    GH_STATE: state,
+    GH_DESCRIPTION: msg,
+    GH_CONTEXT: "brigade",
+    GH_TOKEN: project.secrets.ghToken,
+    GH_COMMIT: e.revision.commit
+  }
+  return gh
+}
